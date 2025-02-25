3f7fbfee04a44a7593deecd7282bb47f579b2932
xitravel <1020074731@qq.com>
2025-02-25 10:12:53 +0800

    Move profile for the first time

---
+++ assignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _2_21_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int a, b = 0;
+            char c='1';
+        
+            Console.WriteLine("please input the first number");
+            s=Console.ReadLine();
+            a=Int32.Parse(s);
+            Console.WriteLine("please input the second number");
+            s = Console.ReadLine();
+            b = Int32.Parse(s);
+            Console.WriteLine("please input the cal");
+            s = Console.ReadLine();
+            c = Char.Parse(s);
+            if (c == '+')
+                Console.WriteLine(a + b);
+            if (c == '-')
+                Console.WriteLine(a - b);
+            if (c == '*')
+                Console.WriteLine(a * b);
+            if (c == '/')
+                Console.WriteLine(a / b);
+        }
+    }
+}
