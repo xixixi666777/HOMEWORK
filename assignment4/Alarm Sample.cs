621e1be5e398bb1b767026218b3e5bbeab39137f
xitravel <1020074731@qq.com>
2025-03-13 21:15:06 +0800

    第四次作业

---
+++ assignment4/Alarm Sample.cs
@@ -0,0 +1,102 @@
+﻿namespace _3_13C__2
+{
+    internal class AlarmSample
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            //set the begin time
+            int startMinute = 0;
+            int startSecond = 0;
+
+            //set the alarm time
+            int alarmMinute = 0;
+            int alarmSecond = 5;
+
+            // start the clock
+            clock.Start(startMinute, startSecond, alarmMinute, alarmSecond);
+        }
+    }
+    public delegate void AlarmSystem(object sender, AlarmEventArgs args);
+    public class AlarmEventArgs { 
+        public int Second {  get; set; }
+        public int Minute { get; set; }
+        public int SetSecond { get; set; }
+        public int SetMinute {  get; set; }
+    }
+    public class Alarm
+    {
+        public event AlarmSystem OnTick;
+        public event AlarmSystem OnAlarm;
+        public void Tick(int x,int y)
+        {
+            Console.WriteLine($"the clock begins at {x}:{y}");
+            AlarmEventArgs args = new AlarmEventArgs()
+            {
+                Second = y,
+                Minute = x
+            };
+            OnTick(this, args);
+        }
+        public void MyAlarm(int x, int y)
+        {
+            Console.WriteLine($"the alarm time is {x}:{y}");
+            AlarmEventArgs args = new AlarmEventArgs()
+            {
+                SetSecond = y,
+                SetMinute = x
+            };
+            OnAlarm(this, args);
+        }
+
+    }
+    public class Clock
+    {
+        public Alarm alarm=new Alarm();
+        public Clock() {
+            alarm.OnTick += new AlarmSystem(My_OnTick);
+            alarm.OnAlarm += My_OnAlarm;
+        }
+        void My_OnTick(object sender,AlarmEventArgs args)
+        {
+            Console.WriteLine($"Tick... Current Time: {args.Minute:00}:{args.Second:00}");
+        }
+        void My_OnAlarm(object sender, AlarmEventArgs args)
+        {
+            Console.WriteLine($"Alarm! Time's up! Current Time: {args.SetMinute:00}:{args.SetSecond:00}");
+        }
+        public void Start(int startMinute, int startSecond, int alarmMinute, int alarmSecond)
+        {
+            int currentMinute = startMinute;
+            int currentSecond = startSecond;
+
+            Console.WriteLine($"start..  Current Time: {currentMinute:00}:{currentSecond:00}");
+            Console.WriteLine($"The alarm time : {alarmMinute:00}:{alarmSecond:00}");
+
+            while (true)
+            {
+                
+                if (currentMinute == alarmMinute && currentSecond == alarmSecond)
+                {
+                    alarm.MyAlarm(currentMinute, currentSecond);
+                    break; 
+                }
+
+                
+                alarm.Tick(currentMinute, currentSecond);
+
+                
+                Thread.Sleep(1000); 
+                currentSecond++;
+
+                
+                if (currentSecond >= 60)
+                {
+                    currentSecond = 0;
+                    currentMinute++;
+                }
+            }
+        }
+    }
+}
---
+++ assignment4/List.cs
@@ -0,0 +1,95 @@
+﻿namespace _3_13C__1
+{
+    internal class List
+    {
+        static void Swap<T>(ref T left, ref T right)
+        {
+            T temp;
+            temp = left;
+            left = right;
+            right = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            GenericList<int>intlist=new GenericList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                intlist.Add(i);
+            }
+            Action<int> PrintElement = (m) => { Console.Write(m+" "); };
+            Console.WriteLine();
+            intlist.ForEach(PrintElement);
+            //calculate the sum
+            int sum = 0;
+            intlist.ForEach(m=>sum+=m);
+            Console.WriteLine($"the sum of the list is {sum}");
+            //calculate the maximum
+            int max = -10;
+            intlist.ForEach(m =>
+            {
+                if (m > max)
+                    max = m;
+            });
+            Console.WriteLine($"the maximum of the list is {max}");
+            //calculate the minimum
+            int min = 100000;
+            intlist.ForEach(m =>
+            {
+                if (m<min)
+                    min = m;
+            });
+            Console.WriteLine($"the minimum of the list is {min}");
+        }
+    }
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t) {
+            Node<T> n = new Node<T>(t);
+            if (tail == null) {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+
+        }
+        public void ForEach(Action<T> action){
+            for (Node<T> node = this.Head; node != null; node = node.Next)
+            {
+
+              action(node.Data);
+            }
+
+
+        }
+        
+    }
+}
+
+
+
+    
+
